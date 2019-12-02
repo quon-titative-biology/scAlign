7d80ef20270a89d8a2a6097f6e6935346c65370f
tbrassel <37461300+tbrassel@users.noreply.github.com>
2019-12-02 11:23:11 -0800

    Add files via upload

---
+++ scAlignClass.R
@@ -0,0 +1,296 @@
+#' Creates scAlign object
+#'
+#' @return Initialized scAlign object
+#'
+#' @param sce.objects List of Seurat or Matrix objects; sample x feature.
+#' @param genes.use Genes to use during PCA/CCA, all genes used as default.
+#' @param labels List of labels for each object.
+#' @param data.use Specificies which data to use from a Seurat object for dimensionality reduction.
+#' @param pca.reduce Initial step of dimensionality be performced by PCA.
+#' @param pcs.compute Number of PCs to retrain for alignment.
+#' @param cca.reduce Initial step of dimensionality be performced by CCA.
+#' @param ccs.compute Number of CCs to retrain for alignment.
+#' @param cca.standardize Standardize the data matrices before CCA.
+#' @param project.name Name for current scAlign project.
+#'
+#' @import SingleCellExperiment
+#' @import irlba
+#' @import Seurat
+#' @import methods
+#' @import utils
+#'
+#' @examples
+#'
+#' library(Seurat)
+#' library(SingleCellExperiment)
+#'
+#' ## Input data, 1000 genes x 100 cells
+#' data = matrix(sample.int(10000, 1000*100, TRUE), 1000, 100)
+#' rownames(data) = paste0("gene", seq_len(1000))
+#' colnames(data) = paste0("cell", seq_len(100))
+#'
+#' age    = c(rep("young",50), rep("old",50))
+#' labels = c(c(rep("type1",25), rep("type2",25)), c(rep("type1",25), rep("type2",25)))
+#'
+#' ctrl.data = data[,which(age == "young")]
+#' stim.data = data[,which(age == "old")]
+#'
+#' ctrlSCE <- SingleCellExperiment(
+#'               assays = list(scale.data = data[,which(age == "young")]))
+#'
+#' stimSCE <- SingleCellExperiment(
+#'               assays = list(scale.data = data[,which(age == "old")]))
+#'
+#' ## Build the scAlign class object and compute PCs
+#' scAlignHSC = scAlignCreateObject(sce.objects = list("YOUNG"=ctrlSCE,
+#'                                                     "OLD"=stimSCE),
+#'                                  labels = list(labels[which(age == "young")],
+#'                                                labels[which(age == "old")]),
+#'                                  pca.reduce = TRUE,
+#'                                  pcs.compute = 50,
+#'                                  cca.reduce = TRUE,
+#'                                  ccs.compute = 15,
+#'                                  project.name = "scAlign_Kowalcyzk_HSC")
+#'
+#' @export
+scAlignCreateObject = function(sce.objects,
+                               genes.use = NULL,
+                               labels = list(),
+                               pca.reduce = FALSE,
+                               pcs.compute = 20,
+                               cca.reduce = FALSE,
+                               ccs.compute = 15,
+                               cca.standardize = TRUE,
+                               data.use = "scale.data",
+                               project.name = "scAlignProject"){
+
+  ## Grab version of scAlign being run
+  version = packageVersion("scAlign")
+  
+  ## Ensure all objects are SCE or converted to SCE 
+  if(all(vapply(sce.objects, class, character(1)) == "Seurat") == TRUE){
+    if(is.null(genes.use)){
+      sce.objects <- as.SingleCellExperimentList(sce.objects)
+    }else{
+      sce.objects <- as.SingleCellExperimentList(sce.objects, genes.use)
+    }
+  }else if(all(vapply(sce.objects, class, character(1)) == "SingleCellExperiment") == FALSE){
+    stop("Unsupported or inconsistent input type(s): Must be Seurat or SingleCellExperiment objects")
+  }
+  
+  ## Ensure data list has names
+  if(is.null(names(sce.objects))){ names(sce.objects) = paste0("dataset", seq_len(length(sce.objects))) }
+  if(!is.list(labels)) { stop("labels must be a list.") }
+
+  ## Combine data into a common SCE
+  combined.sce = Reduce(cbind, sce.objects)
+
+  ## If no gene set provided then use all genes in common across datasets
+  if(is.null(genes.use)){
+    genes.use = rownames(combined.sce)
+  }
+
+  ## Ensure all objects are SCE
+  if(!is(combined.sce, "SingleCellExperiment")){
+    stop("Unsupported or inconsistent input type(s): Must be SingleCellExperiment objects")
+  }
+  ## Determines dataset split
+  group.by=c()
+  for(name in names(sce.objects)){
+    group.by = c(group.by, rep(name, ncol(sce.objects[[name]])))
+  }
+  colData(combined.sce)[,"group.by"] = group.by
+  if(length(labels) == length(sce.objects)){
+    ## Set cell labels
+    tryCatch({
+      ## Set labels to be used in supervised training or defaults
+      colData(combined.sce)[,"scAlign.labels.orig"] = unlist(labels)
+      colData(combined.sce)[,"scAlign.labels"] = if(length(unlist(labels)) == 0) rep("NA", nrow(colData(combined.sce))) else (as.integer(factor(unlist(labels)))-1)
+      colData(combined.sce)[,"scAlign.labels"][is.na(colData(combined.sce)[,"scAlign.labels"])] = -1
+    }, error=function(e){print("Error converting labels to factors."); stop(e);})
+  }else{
+      colData(combined.sce)[,"scAlign.labels"] = rep(0, ncol(combined.sce))
+      print("Ignoring labels list which is either empty or less than the number of sce objects")
+  }
+  metadata(combined.sce)[["arguments"]] = data.frame(encoder.data=character(),
+                                           decoder.data=character(),
+                                           supervised=character(),
+                                           run.encoder=logical(),
+                                           run.decoder=logical(),
+                                           log.dir=character(),
+                                           log.results=logical(),
+                                           device=character(), stringsAsFactors=FALSE)
+  metadata(combined.sce)[["options"]]  = data.frame(steps=integer(),
+                                          batch.size=integer(),
+                                          learning.rate=numeric(),
+                                          log.every=integer(),
+                                          architecture=character(),
+                                          num.dim=integer(),
+                                          perplexity=integer(),
+                                          norm=logical(),
+                                          early.stop=logical(),
+                                          seed=integer(), stringsAsFactors=FALSE)
+
+  ## Ensure data to be used for dimensionality reduction is available
+  if(is.element(data.use, names(assays(combined.sce))) == FALSE){ stop("data.use is not reachable in assays.") }
+
+  ## Reduce to top 50 pcs
+  if(pca.reduce == TRUE){
+    print(paste("Computing partial PCA for top ", pcs.compute, " PCs."))
+    pca_results = irlba(A = t(assay(combined.sce, data.use)[genes.use,]), nv = pcs.compute)
+    pca_mat = pca_results$u
+    colnames(pca_mat) <- paste0("PC", seq_len(pcs.compute))
+    reducedDim(combined.sce, "PCA") = pca_results$u
+    metadata(combined.sce)[["PCA.output"]] = pca_results
+  }
+
+  ## Reduce to top ccs
+  if(cca.reduce == TRUE & (length(sce.objects) == 2)){
+    print(paste("Computing CCA using Seurat."))
+    seurat.objects = list()
+    for(name in names(sce.objects)){
+      seurat.objects[[name]] = CreateSeuratObject(assay(sce.objects[[name]], data.use), project = name)
+      seurat.objects[[name]] = ScaleData(seurat.objects[[name]], do.center=TRUE, do.scale=TRUE)
+    }
+    ## Functional changes due to seurat version
+    if(packageVersion("Seurat") >= 3.0){
+      ## Reduce from gene to cc space
+      combined = RunCCA(seurat.objects[[names(sce.objects)[1]]],
+                        seurat.objects[[names(sce.objects)[2]]],
+                        features=genes.use,
+                        num.cc=ccs.compute,
+                        scale.data=TRUE)
+      ## Load dim reduction into SCE object
+      reducedDim(combined.sce, "CCA") =  Embeddings(combined, reduction = "cca")
+      metadata(combined.sce)[["CCA.output"]] = Loadings(combined, reduction = "cca")
+    }else{
+      ## Reduce from gene to cc space
+      combined = RunCCA(seurat.objects[[names(sce.objects)[1]]],
+                        seurat.objects[[names(sce.objects)[2]]],
+                        genes.use=genes.use,
+                        num.cc=ccs.compute,
+                        scale.data=TRUE)
+      ## Load dim reduction into SCE object
+      reducedDim(combined.sce, "CCA") = GetCellEmbeddings(combined, "cca")
+      metadata(combined.sce)[["CCA.output"]] = GetGeneLoadings(combined, reduction.type = "cca")
+    }
+  }else if(cca.reduce == TRUE & length(sce.objects) > 2){
+    ## Run Multi CCA for input to scAlign
+    multi.cca.input <- lapply(sce.objects, function(seurat.obj){
+        assay(seurat.obj, slot='scale.data')[genes.use,]
+    })
+    reducedDim(combined.sce, "MultiCCA") = RunMultiCCA(multi.cca.input, num.ccs = ccs.compute, standardize = cca.standardize)
+  }
+  return(combined.sce)
+}
+
+#' Set training options
+#'
+#' Defines parameters for optimizer and training procedure.
+#'
+#' @return Options data.frame
+#'
+#' @param steps (default: 15000) Number of training iterations for neural networks.
+#' @param batch.size (default: 150) Number of input samples per training batch.
+#' @param learning.rate (default: 1e-4) Initial learning rate for ADAM.
+#' @param log.every (default: 5000) Number of steps before saving results.
+#' @param architecture (default: "small") Network function name for scAlign.
+#' @param batch.norm.layer (default: FALSE) Include batch normalization in the network structure.
+#' @param dropout.layer (default: TRUE) Include dropout in the network.
+#' @param num.dim (default: 32) Number of dimensions for joint embedding space.
+#' @param perplexity (default: 30) Determines the neighborhood size for each sample.
+#' @param betas (default: 0) Sets the bandwidth of the gaussians to be the same if > 0. Otherwise per cell beta is computed.
+#' @param norm (default: TRUE) Normalize the data mini batches while training scAlign (repeated).
+#' @param full.norm (default: FALSE) Normalize the data matrix prior to scAlign (done once).
+#' @param early.stop (default: TRUE) Early stopping during network training.
+#' @param walker.loss (default: TRUE) Add walker loss to model.
+#' @param reconc.loss (default: FALSE) Add reconstruction loss to model during alignment.
+#' @param walker.weight (default: 1.0) Weight on walker loss component
+#' @param classifier.weight (default: 1.0) Weight on classifier loss component
+#' @param classifier.delay (default: NULL) Delay classifier component of loss function until specific training step. Defaults to (2/3)*steps.
+#' @param gpu.device (default: '0') Which gpu to use.
+#' @param seed (default: 1245) Sets graph level random seed in tensorflow.
+#
+#' @examples
+#'
+#' options=scAlignOptions(steps=15000,
+#'                        log.every=5000,
+#'                        early.stop=FALSE,
+#'                        architecture="large")
+#'
+#' @export
+scAlignOptions = function(steps = 15000, steps.decoder=10000, batch.size = 150,
+                          learning.rate = 1e-4, log.every = 5000,
+                          architecture="large", batch.norm.layer = FALSE, dropout.layer = TRUE,
+                          num.dim = 32, perplexity = 30, betas=0,
+                          norm = TRUE, full.norm = FALSE,
+                          early.stop = FALSE,
+                          walker.loss = TRUE, reconc.loss = FALSE,
+                          walker.weight = 1.0, classifier.weight = 1.0,
+                          classifier.delay=NA, gpu.device = '0',
+                          seed = 1234){
+
+     valid_opts = c("steps", "steps.decoder", "batch.size", "learning.rate", "log.every", "architecture", "batch.norm.layer", "dropout.layer",
+                   "num.dim", "perplexity", "betas", "norm", "full.norm", "early.stop", "walker.loss",
+                   "reconc.loss", "walker.weight", "classifier.weight", "classifier.delay", "gpu.device", "seed")
+     opts = data.frame(steps = steps,
+                       steps.decoder=steps.decoder,
+                       batch.size = batch.size,
+                       learning.rate = learning.rate,
+                       log.every = log.every,
+                       architecture = architecture,
+                       batch.norm.layer = batch.norm.layer,
+                       dropout.layer = dropout.layer,
+                       num.dim = num.dim,
+                       perplexity = perplexity,
+                       betas = betas,
+                       norm = norm,
+                       full.norm = full.norm,
+                       early.stop = early.stop,
+                       walker.loss = walker.loss,
+                       reconc.loss = reconc.loss,
+                       walker.weight = walker.weight,
+                       classifier.weight = classifier.weight,
+                       classifier.delay = classifier.delay,
+                       gpu.device = as.character(gpu.device),
+                       seed = seed,
+                       stringsAsFactors=FALSE)
+    colnames(opts) = valid_opts
+
+    # if(!is.null(options)){
+    #   if(all(names(options) %in% valid_opts)){
+    #     ## Populate options with user supplied parameters
+    #     for(name in names(options)){
+    #       opts[,name] = options[[name]]
+    #     }
+    #   }else{ stop(paste0("These provided options are not valid: ", names(options)[which(!names(options) %in% valid_opts)])) }
+    # }
+    return(opts)
+}
+
+#' Record aguments passed to scAlign
+#'
+#' @return Arguments data.frame
+#'
+#' @keywords internal
+scAlignArguments = function(sce.object,
+                            encoder.data,
+                            decoder.data,
+                            supervised,
+                            run.encoder,
+                            run.decoder,
+                            log.dir,
+                            log.results,
+                            device){
+
+    args = data.frame(encoder.data=encoder.data,
+                      decoder.data=decoder.data,
+                      supervised=supervised,
+                      run.encoder=run.encoder,
+                      run.decoder=run.decoder,
+                      log.dir=normalizePath(log.dir, mustWork=FALSE),
+                      log.results=log.results,
+                      device=device, stringsAsFactors=FALSE)
+    colnames(args) = c("encoder.data", "decoder.data", "supervised", "run.encoder", "run.decoder", "log.dir", "log.results", "device")
+    return(args)
+}
---
+++ scAlignWrapper.R
@@ -0,0 +1,227 @@
+#' Converts a list of Seurat objects into a list of Single Cell Experiment objects
+#' Extends the native Seurat function "as.SingleCellExperiment" by adding the capability to pass lists of Seurat objects
+#' 
+#' @return A correctly initialized SingleCellExperiment object
+#'
+#' @author Tyler Brassel, Nelson Johansen, Gerald Quon
+#' @references Johansen, N., Quon, G. scAlign: a tool for alignment, integration, and rare cell identification from scRNA-seq data.
+#'             Genome Biol 20, 166 (2019) doi: https://doi.org/10.1186/s13059-019-1766-4
+#' @seealso <https://github.com/quon-titative-biology/scAlign>
+#'
+#' @param seurat.obj List of Seurat objects to be converted to Single Cell Experiment objects.
+#' @param genes.use A character vector of gene names which exhibit variable expression across all datasets
+#'
+#' @seealso \code{\link{ExtractGenes}}
+#' @seealso \code{\link{SelectColors}}
+#' @seealso \code{\link{DecoderVariance}}
+#' @seealso \code{\link{MeanDecoderVariance}}
+#' @seealso \code{\link{GetCharMetadata}}
+#'
+#' @examples
+#'
+#' sce.objects = as.SingleCellExperimentList(seurat.obj)
+#' 
+#' @import SingleCellExperiment
+#' @import Seurat
+#' 
+#' @export
+as.SingleCellExperimentList = function(seurat.obj, genes.use){
+  if(is.null(genes.use)){
+    genes.use = as.numeric(nrow(seurat.obj))
+  }else{
+  sce.objects = lapply(seurat.obj, function(seurat.obj){
+    SingleCellExperiment(assays = list(counts = (GetAssayData(object = seurat.obj, slot = "counts"))[genes.use,],
+                                       logcounts = (GetAssayData(object = seurat.obj, slot = "data"))[genes.use,],
+                                       scale.data = (GetAssayData(object = seurat.obj, slot = "scale.data"))[genes.use,]),
+                                       colData = seurat.obj[[]])})
+  return(sce.objects)
+  }
+}
+
+#' Extracts common set of genes across all datasets
+#'
+#' @return A character vector of gene names which exhibit varible expression across all datasets
+#'
+#' @param seurat.obj1 Either a preprocessed Seurat object or a list of Seurat objects
+#' @param seurat.obj2 A preprocessed Seurat object (optional, for simple binary conditions)
+#' 
+#' @seealso \code{\link{as.SingleCellExperimentList}}
+#' @seealso \code{\link{SelectColors}}
+#' @seealso \code{\link{DecoderVariance}}
+#' @seealso \code{\link{MeanDecoderVariance}}
+#' @seealso \code{\link{GetCharMetadata}}
+#' 
+#' @examples
+#' for (i in 1:length(seurat.list)) {
+#'   seurat.list[[i]] <- NormalizeData(seurat.list[[i]])
+#'   seurat.list[[i]] <- ScaleData(seurat.list[[i]], do.scale=T, do.center=T, display.progress=T)
+#'   seurat.list[[i]] <- FindVariableFeatures(seurat.list[[i]], nFeatures = 3000)
+#' }
+#' genes.use <- ExtractGenes(seurat.list)
+#' 
+#' @import SingleCellExperiment
+#' @import Seurat
+#' 
+#' @export
+ExtractGenes = function(seurat.obj1,
+                        seurat.obj2 = NULL){
+  if (all(vapply(seurat.obj1, class, character(1)) == "Seurat") == FALSE) {
+    stop("Unsupported or inconsistent input type(s): Must be Seurat objects")
+  }else if(is.list(seurat.obj1) && is.null(seurat.obj2)){
+    genes.use <- Reduce(intersect, lapply(seurat.obj1, function(seurat.obj1) VariableFeatures(seurat.obj1)))
+  }else if(!is.null(seurat.obj1) && !is.null(seurat.obj2)){
+    genes.use <- Reduce(intersect, list(VariableFeatures(seurat.obj1),
+                                        VariableFeatures(seurat.obj2),
+                                        rownames(seurat.obj1),
+                                        rownames(seurat.obj2)))
+  }else{
+    stop("Please pass either two Seurat objects or a list of Seurat objects")
+  }
+  return(genes.use)
+}
+
+#' Creates a list of unique color values used for plotting
+#'
+#' @return A named vector of unique hexedecimal color values, either generated from a preselected
+#'         vector of 20 unique colors, or from a sequence of colors in hsv colorspace.
+#'
+#' @param seurat.obj A singular preprocessed Seurat object
+#' @param gradient Setting to TRUE will use a sequence of hsv colors instead of 20 unique colors,
+#'                 useful for comparisons of more than 20 cell types.
+#' @param value The Seurat metadata slot to generate colors for. Defaults to "celltype".
+#'
+#' @import SingleCellExperiment
+#' @import Seurat
+#' 
+#' @seealso \code{\link{as.SingleCellExperimentList}}
+#' @seealso \code{\link{ExtractGenes}}
+#' @seealso \code{\link{DecoderVariance}}
+#' @seealso \code{\link{MeanDecoderVariance}}
+#' @seealso \code{\link{GetCharMetadata}}
+#'
+#' @examples
+#' DimPlot(object = seurat.obj,
+#'         reduction = "tsne",
+#'         cols = SelectColors(seurat.obj),
+#'         group.by = "celltype",
+#'         label = TRUE,
+#'         repel = TRUE)
+#' 
+#' @export
+SelectColors <- function(seurat.obj, gradient = FALSE, value = "celltype"){
+  names <- GetCharMetadata(seurat.obj, value = value)
+  if(gradient == FALSE && length(names) <= 20){
+    celltype_colors_unique <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8',
+                                '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+                                '#bcf60c', '#fabebe', '#008080', '#000000',
+                                '#800000', '#aaffc3', '#808000', '#ffd8b1',
+                                '#000075', '#808080', '#e6beff', '#9a6324')
+    colors_use <- celltype_colors_unique[1:length(names)]
+  }else{
+    colors_use <- hsv(seq(0, 1 - 1/(length(names)), length.out = (length(names))))
+  }
+  names(colors_use) <- names
+  return(colors_use)
+}
+
+#' Creates a 2d array of variance values calculated across the 3rd decoder dimension of a 3d array of decoder matrices.
+#' Unless specified, automatically calculates highly variable genes and orders the combined variance matrix in decreasing order, for later graphical analysis analysis.
+#' 
+#' @return A named vector of unique hexedecimal color values, either generated from a preselected
+#'         vector of 20 unique colors, or from a sequence of colors in hsv colorspace.
+#'
+#' @param seurat.obj A singular preprocessed Seurat object
+#' @param gradient Setting to TRUE will use a sequence of hsv colors instead of 20 unique colors,
+#'                 useful for comparisons of more than 20 cell types.
+#' @param names Setting to TRUE will return the celltype data slot as a variable "names"
+#'
+#' @seealso \code{\link{as.SingleCellExperimentList}}
+#' @seealso \code{\link{ExtractGenes}}
+#' @seealso \code{\link{SelectColors}}
+#' @seealso \code{\link{MeanDecoderVariance}}
+#' @seealso \code{\link{GetCharMetadata}}
+#'
+#' @examples
+#' DimPlot(object = seurat.obj,
+#'         reduction = "tsne",
+#'         cols = SelectColors(seurat.obj),
+#'         group.by = "celltype",
+#'         label = TRUE,
+#'         repel = TRUE)
+#'         
+#' @import SingleCellExperiment
+#' @import Seurat
+#' 
+#' @export
+DecoderVariance <- function(all_data_combined,
+                            seurat.obj,
+                            genes.use,
+                            output.length,
+                            decreasing = TRUE,
+                            return.genes = TRUE){
+  all_data_var <- array(data = 0.0, dim = c(as.numeric(dim(all_data_combined)[1]),
+                                            as.numeric(dim(all_data_combined)[2])))
+  rownames(all_data_var) <- GetCharMetadata(seurat.obj = seurat.obj, value = "celltype", unique = FALSE)
+  colnames(all_data_var) <- genes.use
+  for(i in 1:dim(all_data_combined)[1]){
+    for(j in 1:dim(all_data_combined)[2]){
+      data <- c(all_data_combined[i, j, ])
+      var_data <- var(data)
+      all_data_var[i, j] <- var_data
+    }
+  }
+  rm(var_data, data, i, j)
+  if(return.genes == FALSE){
+    list <- list(all_data_var)
+    return(list)
+  }else{
+    high_var_genes <- sort(MeanDecoderVariance(all_data_var = all_data_var), decreasing = decreasing,index.return = T)$ix[1:output.length]
+    list <- list(all_data_var, high_var_genes)
+    return(list)
+  }
+}
+
+#' Calculate mean decoder variance
+#' @return A vector of mean data values for each column of the "all_data_var" parameter.
+#'
+#' @param all_data_var A matrix of variance values calculated across the decoder dimension.
+#'
+#' @seealso \code{\link{as.SingleCellExperimentList}}
+#' @seealso \code{\link{ExtractGenes}}
+#' @seealso \code{\link{SelectColors}}
+#' @seealso \code{\link{DecoderVariance}}
+#' @seealso \code{\link{GetCharMetadata}}
+#' 
+#' @export
+MeanDecoderVariance <- function(all_data_var){
+  all_mean_data <- vector()
+  for(j in 1:dim(all_data_var)[2]){
+    data <- all_data_var[, j]
+    all_mean_data[j] <-  mean(data)
+  }
+  return(all_mean_data)
+}
+
+#' Fetch metadata as a character vector
+#' @return A character vector of the unique values accessed by the Seurat function FetchData
+#'
+#' @param value The name of the internal metadata object the function should access
+#' @param seurat.obj A seurat object
+#' 
+#' @seealso \code{\link{as.SingleCellExperimentList}}
+#' @seealso \code{\link{ExtractGenes}}
+#' @seealso \code{\link{SelectColors}}
+#' @seealso \code{\link{DecoderVariance}}
+#' @seealso \code{\link{MeanDecoderVariance}}
+#'
+#' @import Seurat
+#' 
+#' @export
+GetCharMetadata <- function(seurat.obj, value = "celltype", unique = TRUE){
+  if (unique == TRUE){
+    cell_names <- unlist(c(unique(FetchData(object = seurat.obj, vars = c(value)))))
+  }else if (unique == FALSE){
+    cell_names <- unlist(c(FetchData(object = seurat.obj, vars = c(value))))
+  }
+return(cell_names)
+}
